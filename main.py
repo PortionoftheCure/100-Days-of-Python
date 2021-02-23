6072a0dfc8c795d9170135dedceca1d8702f1600
LuisJackson <portionofthecure@gmail.com>
2021-02-23 03:28:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+#print("What is your name?")
+#input function
+#input("What is your name?")
+#waits for enter to end
+print("Hello " + input("What is your name?"))
