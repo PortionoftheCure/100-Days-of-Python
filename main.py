3f8812d619e191a284f6ed1737de1577cd630720
PortionoftheCure <79443918+PortionoftheCure@users.noreply.github.com>
2021-02-23 20:05:38 -0600

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-# Write your code below this line 👇
-#print("What is your name?")
-#input function
-#input("What is your name?")
-#waits for enter to end
-print("Hello " + input("What is your name?"))
