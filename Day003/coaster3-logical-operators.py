ef0d96638acbdc205f812cf6d5306ab6764eb2d9
PortionoftheCure <79443918+PortionoftheCure@users.noreply.github.com>
2021-03-10 06:35:05 -0600

    Create coaster3-logical-operators.py

---
+++ Day003/coaster3-logical-operators.py
@@ -0,0 +1,41 @@
+# and operator
+# a = 12
+# a > 10 and a < 13
+# true
+
+# or operator
+# a > 13 or a < 13 
+# true
+
+# not operator reverses true/false
+# a = 12
+# not a > 15
+# true
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    print("You're probably dealing with a lot, have a free ride! Still gotta pay for a picture though.")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+  
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+  
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
