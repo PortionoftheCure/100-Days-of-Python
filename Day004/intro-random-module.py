21e26e5097ca79650753c664f467b6259c1d2860
PortionoftheCure <79443918+PortionoftheCure@users.noreply.github.com>
2021-03-10 07:01:11 -0600

    importing/learning random module
    Just notes about random methods and beginning to learn about lists

---
+++ Day004/intro-random-module.py
@@ -0,0 +1,21 @@
+# import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# #random.random #0.0000 - 0.99999....
+# # * 5 gives between 0.0 and 4.999...
+# #random_float * 5
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}.")
+
+#make lists instead of individual variables
+# state1 = "state"
+# state2 = "state2"
+
+states_of_america = ["Delaware", "Alaska", "Mississippi"]
+#ordered
