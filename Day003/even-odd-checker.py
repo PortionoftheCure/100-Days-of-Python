04175f0e46520892ca6922422f1f23c44ba963e3
PortionoftheCure <79443918+PortionoftheCure@users.noreply.github.com>
2021-03-10 06:27:57 -0600

    Create even-odd-checker.py

---
+++ Day003/even-odd-checker.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# % is module, gives remainder of division
+# 7 % 3 would be 1, 7 % 4 would be 3 
+
+#Write your code below this line 👇
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
